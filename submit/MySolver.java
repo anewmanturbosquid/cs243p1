cddc8b31cff5a9916b520819cb8d54cba63a986e
Yang Li <yangli1@myth1.Stanford.EDU>
2014-02-01 23:53:10 -0800

    implemented forward dataflow in MySolver class

--- submit/MySolver.java
+++ submit/MySolver.java
@@ -33,9 +33,63 @@ public class MySolver implements Flow.Solver {
         // this needs to come first.
         analysis.preprocess(cfg);
 
+            System.out.println("Visit CFG.");
         /***********************
          * Your code goes here *
+         1)  compute IN of block using meet operator
+         2)  compute OUT of block using IN and transfer
+         3)  repeat until converge
          ***********************/
+        if (this.analysis.isForward()){
+            System.out.println("Forward dataflow.");
+            boolean outChanged = false;
+            
+            
+              
+            /* iterative alg for a forward data flow */
+            do{
+                outChanged =false;
+                QuadIterator iter = new QuadIterator(cfg, true);
+                while(iter.hasNext()){
+
+                    /* meet with the OUTs of all predecessors to compute IN */
+
+                    java.util.Iterator<Quad> preds = iter.predecessors();
+
+                    Quad currentQuad = (Quad)iter.next();
+                    Flow.DataflowObject oldOut = this.analysis.getOut(currentQuad);
+                    Flow.DataflowObject meetResult = this.analysis.newTempVar();
+
+                    while(preds.hasNext()){
+                        Quad prevQuad =  (Quad)preds.next();
+                        meetResult.meetWith(this.analysis.getOut(prevQuad));
+                    }
+                    this.analysis.setIn(currentQuad,meetResult);
+
+                    /* compute OUT using transfer function */
+
+                    this.analysis.processQuad(currentQuad);
+
+                    /* check changes to OUT */
+                    if (oldOut != this.analysis.getOut(currentQuad)){
+                        outChanged = true;
+                    }
+
+
+                    
+                    
+                }
+
+                /* check whether any OUT changed */
+
+
+            }while(outChanged);
+
+
+        }else{
+            System.out.println("Backward dataflow.");
+        }
+        
 
         // this needs to come last.
         analysis.postprocess(cfg);
